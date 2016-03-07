167cb414390068dec6eb37609a07859ce2fc7603
paarthmadan <madanhockey@hotmail.com>
2016-03-07 18:04:44 -0500

    giphy http request, and added library
    acquired json

--- app/src/main/java/com/example/paarthmadan/giphyapp/MainActivity.java
+++ app/src/main/java/com/example/paarthmadan/giphyapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.paarthmadan.giphyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.loopj.android.http.*;
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                    System.out.println("Success");
+
+                    String json = new String(response);
+
+                    System.out.println(json);
+
+                    System.out.println("Finished");
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
+
     }
 }
