c27b280c77c823be9de04e3593a824ab065e2554
paarthmadan <madanhockey@hotmail.com>
2016-03-07 18:39:56 -0500

    parsed json
    parsed for image url, isolated into variable

--- app/src/main/java/com/example/paarthmadan/giphyapp/MainActivity.java
+++ app/src/main/java/com/example/paarthmadan/giphyapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.paarthmadan.giphyapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.loopj.android.http.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,10 +31,28 @@ public class MainActivity extends AppCompatActivity {
 
                     String json = new String(response);
 
-                    System.out.println(json);
 
-                    System.out.println("Finished");
+                    try {
+                        JSONObject allJSON = new JSONObject(json);
 
+                        System.out.println(allJSON);
+
+                        JSONObject imageData = allJSON.getJSONArray("data").getJSONObject(0).getJSONObject("images");
+
+                        System.out.println(imageData);
+
+                        String image = imageData.getJSONObject("fixed_height").get("url").toString();
+
+                        System.out.println(image);
+
+
+
+
+                    } catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+
+                System.out.println("Finished");
             }
 
             @Override
